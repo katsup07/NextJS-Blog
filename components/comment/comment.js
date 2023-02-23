6eae0b73f5e975a7b3d6a42959cd9b32d6f6c987
katsup <brett_7y7@yahoo.ca>
2023-02-23 13:20:41 +0900

    Add comment notification feedback for user

--- components/comment/comment.js
+++ components/comment/comment.js
@@ -1,51 +1,103 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import classes from './comment.module.css';
 import { postComment } from '../../lib/comments-util';
+import Notification from '../ui/notification';
 
-const Comment = ({updateComments}) => {
+const Comment = ({ updateComments }) => {
 	const [enteredComment, setEnteredComment] = useState('');
 	const [enteredName, setEnteredName] = useState('');
-  const router = useRouter();
-  const {slug: postId} = router.query;
+	const [requestStatus, setRequestStatus] = useState(); // 'pending', 'success', or 'error'
+	const [requestError, setRequestError] = useState('');
+	const router = useRouter();
+	const { slug: postId } = router.query;
 
-  const clearComments = () => {
-    setEnteredComment('');
-    setEnteredName('');
-  }
+	useEffect(() => {
+		if (requestStatus === 'success' || requestStatus === 'error') {
+			const timer = setTimeout(() => setRequestStatus(''), 4000);
+			setRequestError('');
 
-	const commentHandler = (e) => {
+			return () => clearTimeout(timer);
+		}
+	}, [requestStatus]);
+
+	const clearComments = () => {
+		setEnteredComment('');
+		setEnteredName('');
+	};
+
+	const sendCommentHandler = async (e) => {
 		e.preventDefault();
-		console.log('handling comment...', enteredComment, enteredName, postId);
-    postComment({comment: enteredComment, name: enteredName, postId});
-    setTimeout(() => updateComments(), 500); // ensures new comment is already posted to database
-    clearComments();
-   
+		setRequestStatus('pending');
+		try {
+			console.log('handling comment...', enteredComment, enteredName, postId);
+			await postComment({ comment: enteredComment, name: enteredName, postId });
+			setRequestStatus('success');
+			setTimeout(() => updateComments(), 500); // ensures new comment is already posted to database
+			clearComments();
+		} catch (err) {
+			console.log('handling comment error occurred');
+			setRequestStatus('error');
+			setRequestError(err.message);
+		}
+	};
+
+	const notificationJSX = () => {
+		if (requestStatus === 'success')
+			return (
+				<Notification
+					status='success'
+					title='Success'
+					message='Your comment was added.'
+				/>
+			);
+		else if (requestStatus === 'pending')
+			return (
+				<Notification
+					status='pending'
+					title='Pending'
+					message='Trying to add comment...'
+				/>
+			);
+		else if (requestStatus === 'error')
+			return (
+				<Notification
+					status='error'
+					title='Error'
+					message={
+						requestError || 'Something went wrong. Please try again shortly.'
+					}
+				/>
+			);
 	};
 
 	return (
-		<form className={classes.comment} onSubmit={commentHandler}>
-			<div className={classes.controls}>
-				<input
-					id='name'
-					placeholder='Name'
-          value={enteredName}
-          onChange={(e) => setEnteredName(e.target.value)}
-					required/>
-			</div>
-			<div className={classes.controls}>
-				<textarea
-					id='comment'
-					placeholder='Add a comment...'
-          value={enteredComment}
-          onChange={(e) => setEnteredComment(e.target.value)}
-					rows='2'
-          required></textarea>
-			</div>
-			<div className={classes.actions}>
-				<button>comment</button>
-			</div>
-		</form>
+		<>
+			{notificationJSX()}
+			<form className={classes.comment} onSubmit={sendCommentHandler}>
+				<div className={classes.controls}>
+					<input
+						id='name'
+						placeholder='Name'
+						value={enteredName}
+						onChange={(e) => setEnteredName(e.target.value)}
+						required
+					/>
+				</div>
+				<div className={classes.controls}>
+					<textarea
+						id='comment'
+						placeholder='Add a comment...'
+						value={enteredComment}
+						onChange={(e) => setEnteredComment(e.target.value)}
+						rows='2'
+						required></textarea>
+				</div>
+				<div className={classes.actions}>
+					<button>comment</button>
+				</div>
+			</form>
+		</>
 	);
 };
 
